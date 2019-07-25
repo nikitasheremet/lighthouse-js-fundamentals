7f260e492a3d95216b5590e529daa8821751180f
nikitasheremet <nikita.sheremet253@gmail.com>
2019-07-25 19:19:51 +0000

    updated loopy, looping range and merge

--- looping-range.js
+++ looping-range.js
@@ -1,9 +1,14 @@
 function range(start, end, step) {
   let array = [];
-  for (let i = (start); i <= end; i += step) {  
-  array.push(i);
+  for (let i = start; i <= end; i += step) {  
+    if(step <= 0) {
+      break;
+    }
+    array.push(i);
   }
   return array;
 }
 
+console.log(range(-5, -3, -3));
+console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
--- loopylighthouse.js
+++ loopylighthouse.js
@@ -1,11 +1,15 @@
-for (let count = 100; count <=200; count++) {
-  if (count % 3 === 0 && count % 4 ===0) {
-    console.log("LoopyLighthouse");
-  } else if (count % 3 === 0) {
-    console.log("Loopy");
-  } else if (count % 4 === 0) {
-    console.log("Lighthouse");
-  } else {
-    console.log(count);
+function loopyLighthouse(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words.join(''));
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
   }
 }
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
